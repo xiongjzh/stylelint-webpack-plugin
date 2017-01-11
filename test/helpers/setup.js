f8963056ea9092ce2da09f6ea23e9fbd546aae00
Jason Kurian <jasonk92@gmail.com>
2017-01-11 04:10:31 -0500

    chore: removes unused dependency
    and re-resolves packages

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -4,7 +4,5 @@ var chai = require('chai');
 
 global.expect = chai.expect;
 
-chai.use(require('chai-as-promised'));
-
 // get path from the './test' directory
 global.getPath = require('path').join.bind(this, __dirname, '..');
